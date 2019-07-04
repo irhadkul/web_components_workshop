855933eb03417b66685d205790d3078dc792a61b
Irhad Kulanic <irhad.kulanic@adesso.de>
2019-07-04 14:54:13 +0200

    Demos

---
+++ demos/Demo_4-HTMLTemplate/index.final.js
@@ -0,0 +1,13 @@
+
+class DWInput extends HTMLElement {
+    constructor() {
+        super();
+        const template = document.querySelector('#dw-template');
+        const templateContent = template.content;
+        // Shadow DOM
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        // cloneNode is important because we want to make template reusable
+        shadowRoot.appendChild(templateContent.cloneNode(true));
+    }
+}
+window.customElements.define("dw-input", DWInput);
