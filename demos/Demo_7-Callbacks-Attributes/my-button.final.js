00473ecd25bc334bdc869d16c12da49c3ffcef53
Irhad Kulanic <irhad.kulanic@adesso.de>
2019-08-11 20:05:40 +0200

    Demo 7 ueberarbeitet

---
+++ demos/Demo_7-Callbacks-Attributes/my-button.final.js
@@ -0,0 +1,47 @@
+const template = document.createElement("template");
+template.innerHTML = `<button>Klick mich</button>`;
+
+class MyButton extends HTMLElement {
+  static get observedAttributes() {
+    return ["label"];
+  }
+
+  set label(value) {
+    this._label = value;
+    this.buttonElement.innerText = this._label;
+  }
+
+  get label() {
+    return this._label;
+  }
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.buttonElement = this.shadowRoot.querySelector("button");
+    this.buttonElement.addEventListener("click", () => this.handleClick());
+  }
+
+  handleClick() {
+    this.dispatchEvent(
+      new CustomEvent("my-click", { detail: "Ich wurde geklickt!" })
+    );
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    if (attrName === "label") {
+      this.label = newValue;
+    }
+  }
+  connectedCallback() {
+    console.log("Element is connected to the DOM");
+  }
+  disconnectedCallback() {
+    console.log("Element is disconnected from the DOM");
+  }
+}
+
+customElements.define("my-button", MyButton);
