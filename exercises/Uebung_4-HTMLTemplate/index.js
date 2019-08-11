92a8db556c6402130dbb1c8a0dccae19ee74a14c
Irhad Kulanic <irhad.kulanic@adesso.de>
2019-08-11 15:34:50 +0200

    Uebung 4

---
+++ exercises/Uebung_4-HTMLTemplate/index.js
@@ -0,0 +1,27 @@
+/**
+ * 1. Lagert alle Kindelemente aus der MyContactCard in ein Template aus.
+ * - Das Template soll als String in die JS-Variable implementiert und an die shadowRoot angehängt werden.
+ * */
+class MyContactCard extends HTMLElement {
+  constructor() {
+    super();
+    const pElement = document.createElement("p");
+    const imgElement = document.createElement("img");
+    const divHolder = document.createElement("div");
+    // Style
+    const style = document.createElement("style");
+    style.textContent = `img { max-width:200px; }`;
+
+    imgElement.src = "../global/images/img1.png";
+    pElement.textContent = "Name: Alex Müller";
+    divHolder.appendChild(pElement);
+
+    // Shadow DOM
+    const shadowRoot = this.attachShadow({ mode: "open" });
+
+    shadowRoot.appendChild(style);
+    shadowRoot.appendChild(imgElement);
+    shadowRoot.appendChild(divHolder);
+  }
+}
+window.customElements.define("my-contact-card", MyContactCard);
