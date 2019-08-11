2a53787f17b542a9bee2cb8b5f748f975d95497c
Irhad Kulanic <irhad.kulanic@adesso.de>
2019-08-11 14:20:10 +0200

    exercieses dient nur als vorlaeufiges ordner fürs neue uebungen struktur

---
+++ exercises/Uebung_1-CustomElements/index.js
@@ -0,0 +1,13 @@
+/**
+ * 01. TODO - Ein Klassenobjekt mit dem Namen MyContactCard definieren.
+ * Das Custom Element soll zwei Kind-Elemente enthalten:
+ * ein img-Element zur Anzeige eines Bildes und
+ * ein p-Element zur Darstellung des Namens.
+ *
+ * HINWEISE:
+ * const pEl = document.createElement('p') - erzeugt ein p-Element
+ * this.appendChild(pEl) - fügt das p-Element als Kind-Element hinzu
+ *
+ * 02. TODO - customElements.define() nutzen, um das neue Element zu registrieren
+ * 03. TODO - Das JavaScript des Custom Elements in die index.html als ES-Module einbinden und den neuen HTML-Tag verwenden
+ */
